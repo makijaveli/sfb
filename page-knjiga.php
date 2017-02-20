2d545180c34f329cc511462a58b2c4376f68f29b
Miloš Avramović <Milo Avramovi>
2017-02-20 23:08:54 +0100

    MA test

--- page-knjiga.php
+++ page-knjiga.php
@@ -44,9 +44,9 @@ Template Name: Knjiga
 			
 			
 			<?php if ( is_active_sidebar( 'widget-area-1' ) ) : ?>
-				<div id="primary-sidebar" class="primary-sidebar widget-area" role="complementary">
+				 <div id="primary-sidebar" class="primary-sidebar widget-area" role="complementary">
 					<?php dynamic_sidebar( 'widget-area-1' ); ?>
-				</div><!-- #primary-sidebar -->
+				</div><!-- #primary-sidebar  -->
 			<?php endif; ?>
 
 		</aside>
