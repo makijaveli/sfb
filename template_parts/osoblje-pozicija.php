dd0d8e83a15d5e144cc7858f6abe70e3cf535963
mwebrs <kontakt@mweb.rs>
2017-02-25 13:26:50 +0100

    Izmena

---
+++ template_parts/osoblje-pozicija.php
@@ -0,0 +1,13 @@
+<?php if(in_category(17)): ?>
+    <span><?php _e( 'Редовни професор', 'mweb' ); ?></span>
+<?php elseif(in_category(18)): ?>
+    <span><?php _e( 'Ванредни професор', 'mweb' ); ?></span>
+<?php elseif(in_category(19)): ?>
+    <span><?php _e( 'Доцент', 'mweb' ); ?></span>
+<?php elseif(in_category(20)): ?>
+    <span><?php _e( 'Асистент', 'mweb' ); ?></span>
+<?php elseif(in_category(21)): ?>
+    <span><?php _e( 'Сарадник у настави', 'mweb' ); ?></span>					
+<?php elseif(in_category(31)): ?>
+    <span><?php _e( 'Стручни сарадник', 'mweb' ); ?></span>			
+<?php endif; ?>
