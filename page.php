495607b221abee2b65041ef5a4ad1a3c2ca16e81
mwebrs <kontakt@mweb.rs>
2017-03-13 22:27:48 +0100

    Izmene

--- page.php
+++ page.php
@@ -65,15 +65,12 @@ if (is_tree(718)) {
 
 	<main>
 
-		<aside class="page <?php if ( (is_tree(718)) ) echo 'red'; elseif ( (is_tree(1085)) ) echo 'orange'; elseif ( (is_tree(1083)) ) echo 'blue'; elseif ( (is_tree(2628)) ) echo 'green'; ?>">
-			
-			
+		<aside class="page <?php if ( (is_tree(718)) ) echo 'red'; elseif ( (is_tree(1085)) ) echo 'orange'; elseif ( (is_tree(1083)) ) echo 'blue'; elseif ( (is_tree(2628)) ) echo 'green'; ?>">				
 			<?php if ( is_active_sidebar( 'widget-area-1' ) ) : ?>
 				<div id="primary-sidebar" class="primary-sidebar widget-area" role="complementary">
 					<?php dynamic_sidebar( 'widget-area-1' ); ?>
 				</div><!-- #primary-sidebar -->
 			<?php endif; ?>
-
 		</aside>
 
 		<div class="page-content <?php if ( (is_tree(718)) ) echo 'red'; elseif ( (is_tree(1085)) ) echo 'orange'; elseif ( (is_tree(1083)) ) echo 'blue'; elseif ( (is_tree(2628)) ) echo 'green'; ?>">
