0c97a3cf7817ffb83e943799eb5d70abb649ae3f
Milos Avramovic <avramovic.m@oilpc.ru>
2017-03-15 17:11:34 +0100

    Sidebar

--- sidebar.php
+++ sidebar.php
@@ -1,8 +1,8 @@
-<?php if(is_page(array(10))): ?>
+<?php if(is_page(array(1826))): ?>
 		<aside class="page <?php if ( (is_tree(718)) ) echo 'red'; elseif ( (is_tree(1085)) ) echo 'orange'; elseif ( (is_tree(1083)) ) echo 'blue'; elseif ( (is_tree(2628)) ) echo 'green'; ?>">				
-			<?php if ( is_active_sidebar( 'widget-area-2' ) ) : ?>
+			<?php if ( is_active_sidebar( 'widget-area-4' ) ) : ?>
 				<div id="primary-sidebar" class="primary-sidebar widget-area" role="complementary">
-					<?php dynamic_sidebar( 'widget-area-2' ); ?>
+					<?php dynamic_sidebar( 'widget-area-4' ); ?>
 				</div><!-- #primary-sidebar -->
 			<?php endif; ?>
 		</aside>
