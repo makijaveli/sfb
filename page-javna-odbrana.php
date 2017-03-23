2110c871f2bf567ba0313bb9dbfd083448214ba9
mwebrs <kontakt@mweb.rs>
2017-03-23 22:48:16 +0100

    Test
    Opis testa

--- page-javna-odbrana.php
+++ page-javna-odbrana.php
@@ -46,7 +46,7 @@ get_header();
 			<?php if ( is_active_sidebar( 'widget-area-1' ) ) : ?>
 				<div id="primary-sidebar" class="primary-sidebar widget-area" role="complementary">
 					<?php dynamic_sidebar( 'widget-area-1' ); ?>
-				</div><!-- #primary-sidebar -->
+				</div><!-- #primary-sidebar  -->
 			<?php endif; ?>
 
 		</aside>
