fbb26e74091c3aaedcf00b3507449a5f7066a9dc
Miloš Avramović <Milo Avramovi>
2017-03-23 19:59:40 +0100

    Sidebar za studente

--- sidebar.php
+++ sidebar.php
@@ -19,7 +19,9 @@
 	|| in_array(2770, $post->ancestors) 
 	|| in_array(1805, $post->ancestors) 
 	|| in_array(1818, $post->ancestors) 
-	|| in_array(1801, $post->ancestors) 
+	|| in_array(1801, $post->ancestors)
+	|| in_array(1402, $post->ancestors)
+	|| is_page(1402) 
 	): ?>
 
    <aside class="page <?php if ( (is_tree(718)) ) echo 'red'; elseif ( (is_tree(1085)) ) echo 'orange'; elseif ( (is_tree(1083)) ) echo 'blue'; elseif ( (is_tree(2628)) ) echo 'green'; ?>">				
