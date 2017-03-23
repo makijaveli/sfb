0501d52084c34791bf01059c64a3f62825645d3b
Miloš Avramović <Milo Avramovi>
2017-03-23 22:09:32 +0100

    Commit category

--- category.php
+++ category.php
@@ -38,36 +38,6 @@ get_header();
 	<main>
 
 		<aside class="page-profesor">
-			
-			<h3>
-				<?php _e( 'Најновије вести', 'sfb' ); ?>
-			</h3>	
-			
-			<div class="cataside">
-
-			<?php 
-				// the query
-				$wpb_all_query = new WP_Query(array('post_type'=>'post', 'category__in' => array(2,3), 'post_status'=>'publish', 'posts_per_page'=> 5)); ?>
-
-				<?php if ( $wpb_all_query->have_posts() ) : ?>
-
-				<ul>
-
-					<!-- the loop -->
-					<?php while ( $wpb_all_query->have_posts() ) : $wpb_all_query->the_post(); ?>
-						<li><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></li>
-					<?php endwhile; ?>
-					<!-- end of the loop -->
-
-				</ul>
-
-					<?php wp_reset_postdata(); ?>
-
-				<?php else : ?>
-					<p><?php _e( 'Sorry, no posts matched your criteria.' ); ?></p>
-				<?php endif; ?>
-
-			</div>
 
 			<div class="cataside">
 
