0ebeff60807ee25323259b57c47c0573351452a4
mwebrs <marcomilic@mwe.rs>
2017-04-18 00:24:05 +0200

    Izmena postavki linkova

--- template_parts/cards-sp.php
+++ template_parts/cards-sp.php
@@ -3,56 +3,42 @@ $link_su = get_field('link_su');
 $link_tmp = get_field('link_tmp');
 $link_paih = get_field('link_paih');
 $link_ei = get_field('link_ei');
-
 ?>
 
-<div class="cards studpro">
-				
-				<div class="card sp green">
-					
-					<div class="cardtitle studpro">
+<div class="cards studpro">				
+	<?php if($link_su): ?>
+	<div class="card sp green">					
+		<div class="cardtitle studpro">
+            <span><?php _e( 'Студијски програм', 'sfb' ); ?></span>
+			<h2><a href="<?php echo $link_su; ?>"><?php _e( 'Шумарство', 'sfb' ); ?></a></h2>	
+		</div>
+	</div>
+	<?php endif; ?>
 
-                        <span><?php _e( 'Студијски програм', 'sfb' ); ?></span>
-						<h2><a href="<?php echo $link_su; ?>"><?php _e( 'Шумарство', 'sfb' ); ?></a></h2>
-	
-					</div>
+	<?php if($link_tmp): ?>
+	<div class="card sp orange">					
+		<div class="cardtitle studpro">
+            <span><?php _e( 'Студијски програм', 'sfb' ); ?></span>
+			<h2><a href="<?php echo $link_tmp; ?>"><?php _e( 'Технологије, менаџмент и пројектовање намештаја и производа од дрвета', 'sfb' ); ?></a></h2>
+		</div>
+	</div>
+	<?php endif; ?>
 
-				</div>
+	<?php if($link_paih): ?>
+	<div class="card sp red">					
+		<div class="cardtitle studpro">
+            <span><?php _e( 'Студијски програм', 'sfb' ); ?></span>
+			<h2><a href="<?php echo $link_paih; ?>"><?php _e( 'Пејзажна архитектура', 'sfb' ); ?></a></h2>
+		</div>
+	</div>
+	<?php endif; ?>
 
-				<div class="card sp orange">
-					
-					<div class="cardtitle studpro">
-
-                        <span><?php _e( 'Студијски програм', 'sfb' ); ?></span>
-
-						<h2><a href="<?php echo $link_tmp; ?>"><?php _e( 'Технологије, менаџмент и пројектовање намештаја и производа од дрвета', 'sfb' ); ?></a></h2>
-
-					</div>
-
-				</div>
-
-				<div class="card sp red">
-					
-					<div class="cardtitle studpro">
-
-                        <span><?php _e( 'Студијски програм', 'sfb' ); ?></span>
-
-						<h2><a href="<?php echo $link_paih; ?>"><?php _e( 'Пејзажна архитектура', 'sfb' ); ?></a></h2>
-
-					</div>
-
-				</div>
-
-				<div class="card sp blue">
-					
-					<div class="cardtitle studpro">
-
-                        <span><?php _e( 'Студијски програм', 'sfb' ); ?></span>
-
-						<h2><a href="<?php echo $link_ei; ?>"><?php _e( 'Еколошки инжењеринг у заштити земљишних и водeних ресурса', 'sfb' ); ?></a></h2>
-
-					</div>
-
-				</div>
-
-			</div><!-- cards -->
+	<?php if($link_ei): ?>
+	<div class="card sp blue">					
+		<div class="cardtitle studpro">
+            <span><?php _e( 'Студијски програм', 'sfb' ); ?></span>
+			<h2><a href="<?php echo $link_ei; ?>"><?php _e( 'Еколошки инжењеринг у заштити земљишних и водeних ресурса', 'sfb' ); ?></a></h2>
+		</div>
+	</div>
+	<?php endif; ?>
+</div><!-- cards -->
