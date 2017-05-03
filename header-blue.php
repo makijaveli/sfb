e200bc835cc8019873638bfa7568e76f4dd392cf
mwebrs <marcomilic@mwe.rs>
2017-05-03 17:04:47 +0200

    Uklonjen višak koda

--- header-blue.php
+++ header-blue.php
@@ -151,9 +151,7 @@
 			<ul>
 			    <li><a href=""><i class="fa fa-facebook-square" aria-hidden="true"></i></a></li>
 				<li><a href=""><i class="fa fa-twitter-square" aria-hidden="true"></i></a></li>
-				<li><a href=""><i class="fa fa-youtube-play" aria-hidden="true"></i></a></li>
-				<li><a href=""><i class="fa fa-pinterest-square" aria-hidden="true"></i></a></li>
-				<li><a href=""><i class="fa fa-google-plus-square" aria-hidden="true"></i></a></li>
+				<li><a href=""><i class="fa fa-youtube-play" aria-hidden="true"></i></a></li>				
 			</ul>
 
 			
