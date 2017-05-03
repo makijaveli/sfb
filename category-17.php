797384b42bcf640c6e74713ce413f9c4c1fe5dc2
mwebrs <marcomilic@mwe.rs>
2017-05-03 16:34:51 +0200

    Sređen kod - smanjeni razmaci

--- category-17.php
+++ category-17.php
@@ -1,51 +1,29 @@
-<?php 
+<?php get_header(); ?>
 
-get_header();
-
-?>
-
-<div class="breadcrumbs">
-	
+<div class="breadcrumbs">	
 	<div class="wrapper">
-
 		<div class="breadcrumb">
-
 			<?php $url = home_url();?>
-
-			<a href="<?php echo $url; ?>"> 
-
-			<?php _e( 'Шумaрски факултет', 'sfb' ); ?>
-
+			<a href="<?php echo $url; ?>">
+				<?php _e( 'Шумaрски факултет', 'sfb' ); ?>
 			</a> >
 
-			 <?php if(function_exists('bcn_display'))
+			<?php if(function_exists('bcn_display'))
 	    	{
-
 	        bcn_display();
-
 	    	} ?>
-
     	</div>
-
-
-	</div>
-
+    </div>
 </div>
 
 <div class="wrapper">
-	
-
 	<main>
-
-		<aside class="page-profesor">
-			
-		<?php if ( is_active_sidebar( 'widget-area-2' ) ) : ?>
+		<aside class="page-profesor">			
+			<?php if ( is_active_sidebar( 'widget-area-2' ) ) : ?>
 				<div id="primary-sidebar" class="primary-sidebar widget-area" role="complementary">
 					<?php dynamic_sidebar( 'widget-area-2' ); ?>
 				</div><!-- #primary-sidebar -->
-		<?php endif; ?>
-		    
-
+			<?php endif; ?>
 		</aside>
 
 		<div class="content">
@@ -53,14 +31,9 @@ get_header();
 		</div>
 
 		<div class="page-content">
-
-				<?php get_template_part('template_parts/catprof-template'); ?>
-
-     	</div><!-- page-content -->
-	
+			<?php get_template_part('template_parts/catprof-template'); ?>
+     	</div><!-- page-content -->	
 	</main>
-
 </div><!-- wrapper -->
-
 
 <?php get_footer(); ?>
