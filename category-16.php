f380c26a68cf36e7d6f492b27ed8bd7ce98b4668
mwebrs <marcomilic@mwe.rs>
2017-05-03 16:32:22 +0200

    Smanjeni razmaci izmenju redova

--- category-16.php
+++ category-16.php
@@ -1,51 +1,28 @@
-<?php 
+<?php get_header(); ?>
 
-get_header();
-
-?>
-
-<div class="breadcrumbs">
-	
+<div class="breadcrumbs">	
 	<div class="wrapper">
-
 		<div class="breadcrumb">
-
 			<?php $url = home_url();?>
-
 			<a href="<?php echo $url; ?>"> 
-
-			<?php _e( 'Шумaрски факултет', 'sfb' ); ?>
-
+				<?php _e( 'Шумaрски факултет', 'sfb' ); ?>
 			</a> >
-
-			 <?php if(function_exists('bcn_display'))
+			<?php if(function_exists('bcn_display'))
 	    	{
-
 	        bcn_display();
-
 	    	} ?>
-
     	</div>
-
-
-	</div>
-
+    </div>
 </div>
 
 <div class="wrapper">
-	
-
 	<main>
-
-		<aside class="page-profesor">
-			
-		<?php if ( is_active_sidebar( 'widget-area-2' ) ) : ?>
-				<div id="primary-sidebar" class="primary-sidebar widget-area" role="complementary">
-					<?php dynamic_sidebar( 'widget-area-2' ); ?>
-				</div><!-- #primary-sidebar -->
-		<?php endif; ?>
-		    
-
+		<aside class="page-profesor">			
+			<?php if ( is_active_sidebar( 'widget-area-2' ) ) : ?>
+					<div id="primary-sidebar" class="primary-sidebar widget-area" role="complementary">
+						<?php dynamic_sidebar( 'widget-area-2' ); ?>
+					</div><!-- #primary-sidebar -->
+			<?php endif; ?>
 		</aside>
 
 		<div class="content">
@@ -53,14 +30,9 @@ get_header();
 		</div>
 
 		<div class="page-content">
-
-				<?php get_template_part('template_parts/catprof-template'); ?>
-
-     	</div><!-- page-content -->
-	
+			<?php get_template_part('template_parts/catprof-template'); ?>
+     	</div><!-- page-content -->	
 	</main>
-
 </div><!-- wrapper -->
-
 
 <?php get_footer(); ?>
