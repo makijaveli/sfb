9fb2f3b97e02c3beae51a8a62da6fb3f7fd2ab68
Makijaveli <makkya@gmail.com>
2017-05-05 00:36:47 +0200

    breadcrumbs na single

--- single-96.php
+++ single-96.php
@@ -4,6 +4,34 @@ get_header();
 
 ?>
 
+<div class="breadcrumbs">
+	
+	<div class="wrapper">
+
+		<div class="breadcrumb">
+
+			<?php $url = home_url();?>
+
+			<a href="<?php echo $url; ?>"> 
+
+			<?php _e( 'Шумарски факултет', 'sfb' ); ?>
+
+			</a> >
+
+			 <?php if(function_exists('bcn_display'))
+	    	{
+
+	        bcn_display();
+
+	    	} ?>
+
+    	</div>
+
+
+	</div>
+
+</div>
+
 <div class="wrapper">
 	
 
